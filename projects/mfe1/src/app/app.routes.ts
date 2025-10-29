9294f5648039abcfe27e5593899a18675996042d
abdallah_elmesery <abdallah_elmesery@efinance.com.eg>
2025-10-29 16:07:40 +0300

    clean code

--- projects/mfe1/src/app/app.routes.ts
+++ projects/mfe1/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { MfeHome } from './../../mfe-home/mfe-home';
+import { MfeHome } from './mfe-home/mfe-home';
 export const routes: Routes = [
     {
     path: '',
